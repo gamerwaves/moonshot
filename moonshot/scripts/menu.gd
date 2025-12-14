3504ddc83a5840298973d19ae9728f4ee029d76d
neon443 <69979447+neon443@users.noreply.github.com>
2025-12-14 09:35:52 -0500

    link menu with the starfall level

--- moonshot/scripts/menu.gd
+++ moonshot/scripts/menu.gd
@@ -3,3 +3,7 @@ extends Node
 
 func _on_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
+
+
+func startStargazing() -> void:
+	get_tree().change_scene_to_file("res://scenes/starfall.tscn")
