d8728dbea5ae16d66b1c1cad01107b555efe3fd7
neon443 <69979447+neon443@users.noreply.github.com>
2025-12-15 04:01:03 -0500

    added black hole

---
+++ moonshot/scenes/blak_hole.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var fall_speed := 200
+	
+func _process(delta):
+	position.y += fall_speed * delta
+
+	if position.y > 658 + 10:
+		position.x = randi_range(10, 1142)
+		position.y = 0
+
+func _on_body_entered(body: Node2D) -> void:
+	OS.kill(1)
