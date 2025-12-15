35a66dc9d24cbacfbabd1118dcf6122d453f3f04
neon443 <69979447+neon443@users.noreply.github.com>
2025-12-15 03:40:36 -0500

    merge confligt

--- moonshot/scripts/menu.gd
+++ moonshot/scripts/menu.gd
@@ -2,7 +2,7 @@ extends Node
 
 
 func _on_button_button_up() -> void:
-	get_tree().change_scene_to_file("res://scenes/main.tscn")
+	get_tree().change_scene_to_file("res://scenes/two.tscn")
 
 
 func startStargazing() -> void:
